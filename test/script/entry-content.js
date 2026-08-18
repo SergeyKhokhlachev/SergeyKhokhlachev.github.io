780272b36ccb5e8a674f493db07c734acce47e47
Sergey <sergey.khokhlachev@inbox.ru>
2026-08-18 20:07:09 +0300

    Delete test directory

--- test/script/entry-content.js
+++
@@ -1,2 +0,0 @@
-import"./chunk/chunk-core.js";window.addEventListener("DOMContentLoaded",()=>{console.log("content")});
-//# sourceMappingURL=entry-content.js.map
--- test/script/entry-ui-kit.js
+++
@@ -1,2 +0,0 @@
-import"./chunk/chunk-core.js";window.addEventListener("DOMContentLoaded",()=>{console.log("uikit")});
-//# sourceMappingURL=entry-ui-kit.js.map
