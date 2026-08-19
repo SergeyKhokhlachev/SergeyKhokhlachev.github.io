1d370f5d9da02347c9c873bec5ff9e1b6d833efc
Sergey Khokhlachev <sergey.khokhlachev@inbox.ru>
2026-08-20 01:48:35 +0300

    update  path

--- builder-mpa/script/chunk-get-cart-Qaea6H6W.js
+++ builder-mpa/script/chunk-get-cart-Qaea6H6W.js
@@ -1,2 +1,2 @@
-async function e(){return fetch(`../data/cart.json`).then(e=>e.json()).then(e=>e).catch(e=>e)}export{e as t};
+async function e(){return fetch(`/builder-mpa/data/cart.json`).then(e=>e.json()).then(e=>e).catch(e=>e)}export{e as t};
 //# sourceMappingURL=chunk-get-cart-Qaea6H6W.js.map
